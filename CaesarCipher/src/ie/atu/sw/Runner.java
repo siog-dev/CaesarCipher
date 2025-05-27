340149e79ba0ff0a80ab161663edc9d9f5df18d6
siog <siog@LAPTOP-545TJ6PE>
2025-05-27 21:33:56 +0100

    Get the decrypted stuff

--- CaesarCipher/src/ie/atu/sw/Runner.java
+++ CaesarCipher/src/ie/atu/sw/Runner.java
@@ -4,9 +4,11 @@ public class Runner {
 
 	public static void main(String[] args) {
 		CaesarCipher cc = new CaesarCipher(7);
+		
 		// Get some encrypted stuff out
 		String encrypted = cc.encrypt("Gauls are smelly and dangerous");
 		System.out.println(encrypted);
+		System.out.println(cc.decrypt(encrypted));
 	}
 	
 }
