bc6fd5db518be3758533d8a1ec66b2c0ec904c24
siog <siog@LAPTOP-545TJ6PE>
2025-05-27 21:08:32 +0100

    Create an instance var as the key to shift letters

--- CaesarCipher/src/ie/atu/sw/CaesarCipher.java
+++ CaesarCipher/src/ie/atu/sw/CaesarCipher.java
@@ -1,5 +1,12 @@
 package ie.atu.sw;
 
 public class CaesarCipher {
-
+	// We need a key. Ceasar Cipher is a simple symmetric cipher
+	/*
+	 * A ceasar cipher is a simple symmetric/substitution cipher
+	 * Uses the same key to encrypt and decrypt
+	 */
+	private int key = 0; // Int key -> the number is the key/letter shift
+	
+	
 }
