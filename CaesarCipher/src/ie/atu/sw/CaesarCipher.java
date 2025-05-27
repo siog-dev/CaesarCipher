4fe2135177cdf59b69fec80a37f51d6f8f5e1e89
siog <siog@LAPTOP-545TJ6PE>
2025-05-27 22:28:35 +0100

    Remove study note comments

--- CaesarCipher/src/ie/atu/sw/CaesarCipher.java
+++ CaesarCipher/src/ie/atu/sw/CaesarCipher.java
@@ -1,32 +1,25 @@
 package ie.atu.sw;
 
 public class CaesarCipher {
-	/*
-	 * A ceasar cipher is a simple symmetric/substitution cipher
-	 * Uses the same key to encrypt and decrypt
-	 */
-	private int key = 0; // Int key -> the number is the key/letter shift
+
+	private int key = 0;
 
 	public CaesarCipher(int key) {
-		// super();
 		this.key = key;
 	}
 	
-	public String encrypt(String plainText) { // Text anyone can read
-		// Loop over each char in the string, and offset it by the key
+	public String encrypt(String plainText) {
 		String enc = "";
 		
 		for (int i = 0; i < plainText.length(); i++) {
-			// Use charAt(int index) to get a char string
-			// Use codePointAt(int index) to get an int (unicode int)
 			char ch = (char) (plainText.codePointAt(i) + key);
-			enc = enc + ch; // This is bad form - don't do this in a loop
+			enc = enc + ch;
 		}
 		
 		return enc;
 	}
 	
-	public String decrypt(String cipherText) { // Encrypted text
+	public String decrypt(String cipherText) {
 		String dec = "";
 		
 		for (int i = 0; i < cipherText.length(); i++) {
--- CaesarCipher/src/ie/atu/sw/Runner.java
+++ CaesarCipher/src/ie/atu/sw/Runner.java
@@ -5,10 +5,8 @@ public class Runner {
 	public static void main(String[] args) {
 		CaesarCipher cc = new CaesarCipher(7);
 		
-		// Get some encrypted stuff out
 		String encrypted = cc.encrypt("Gauls are smelly and dangerous");
 		System.out.println(encrypted);
 		System.out.println(cc.decrypt(encrypted));
 	}
-	
 }
