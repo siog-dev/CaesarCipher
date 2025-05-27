b673d8e95917752a1d297c3b3008e7d6bf6daa37
siog <siog@LAPTOP-545TJ6PE>
2025-05-27 21:30:59 +0100

    Get some encrypted stuff out

--- CaesarCipher/src/ie/atu/sw/Runner.java
+++ CaesarCipher/src/ie/atu/sw/Runner.java
@@ -4,6 +4,9 @@ public class Runner {
 
 	public static void main(String[] args) {
 		CaesarCipher cc = new CaesarCipher(7);
+		// Get some encrypted stuff out
+		String encrypted = cc.encrypt("Gauls are smelly and dangerous");
+		System.out.println(encrypted);
 	}
 	
 }
