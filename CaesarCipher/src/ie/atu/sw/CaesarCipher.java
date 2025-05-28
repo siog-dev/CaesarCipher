10e1a64649e0fc62d3c7e4d4a1ac835848e74743
siog <siog@LAPTOP-545TJ6PE>
2025-05-28 12:02:20 +0100

    Remove study note comments

--- CaesarCipher/src/ie/atu/sw/CaesarCipher.java
+++ CaesarCipher/src/ie/atu/sw/CaesarCipher.java
@@ -8,22 +8,9 @@ public class CaesarCipher {
 		this.key = key;
 	}
 	
-	/*
-	 * 2. REMOVE REPITITION
-	 * 
-	 * Create a private method to call once
-	 * Keep signature "public String encrypt(String plainText)"
-	 * Call builder as a the new method and pass it true or false
-	 * True - encrypt, false - decrypt
-	 */
-	
-	// private String transform(String s, boolean encrypt)
 	private String transform(String s, boolean encrypt) {
 		StringBuilder sb = new StringBuilder("");
-		// Hard coding enc = true means it will always encrypt!
-		// boolean enc = true;
-		
-		// Check input string s, not sb - sb is currently empty
+
 		for (int i = 0; i < s.length(); i++) {
 			if (encrypt) {
 				char ch = (char) (s.codePointAt(i) + key);
@@ -43,30 +30,4 @@ public class CaesarCipher {
 	public String decrypt(String s) {
 		return transform(s, false);
 	}
-	
-	/*
-	public String encrypt(String plainText) {
-		StringBuilder sb = new StringBuilder("");
-		
-		for (int i = 0; i < plainText.length(); i++) {
-			char ch = (char) (plainText.codePointAt(i) + key);
-			
-			sb.append(ch);
-		}
-		
-		return sb.toString();
-	}
-	
-	public String decrypt(String cipherText) {
-		StringBuilder sb = new StringBuilder("");
-		
-		for (int i = 0; i < cipherText.length(); i++) {
-			char ch = (char) (cipherText.codePointAt(i) - key);
-			sb.append(ch);
-		}
-		
-		return sb.toString();
-		
-	}
-	*/
 }
